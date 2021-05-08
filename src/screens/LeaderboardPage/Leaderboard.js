fd727bd97c24815d1e537e5616cac9e072f129dd
zdandrew <zdxandrew@gmail.com>
2021-05-07 21:59:44 -0400

    connected leaderboard to firebase

--- src/screens/LeaderboardPage/Leaderboard.js
+++ src/screens/LeaderboardPage/Leaderboard.js
@@ -1,6 +1,11 @@
-import React from "react";
+
 import '../../style/Leaderboard.css';
 import defaultProfile from '../../images/default-profile.png';
+import firebase from "firebase/app";
+import "firebase/auth";
+import config from '../SignUpPage/config';
+import { IfFirebaseAuthed, IfFirebaseUnAuthed, FirebaseAuthProvider } from "@react-firebase/auth";
+import React,{useState,useEffect} from 'react';
 
 const exampleData = [
   { id: 1, picture: defaultProfile, name: "jim", score: 50.00 },
@@ -8,10 +13,25 @@ const exampleData = [
   { id: 3, picture: defaultProfile, name: "patrick", score: 25.00 }
 ]
 
-
-
 function Leaderboard() {
-  exampleData.sort((a, b) => b.score - a.score)
+  var realData = [];
+  var db = firebase.firestore();
+  exampleData.sort((a, b) => b.score - a.score);
+  const [user,setUser]=useState([])
+  const fetchUser=async()=>{
+    const response=db.collection('user');
+    const data=await response.get();
+    data.docs.forEach(item=>{
+     setUser(user => [...user, {name: item.data().name, score: item.data().score, picture: defaultProfile}]);
+     console.log(item.data().score);
+
+     
+    })
+  }
+  useEffect(() => {
+    fetchUser();
+  }, [])
+  user.sort((a, b) => b.score - a.score);
   return (
     <>
       <div id="container">
@@ -25,11 +45,12 @@ function Leaderboard() {
               <th id="name-col"> Name </th>
               <th id="score-col"> Score </th>
             </tr>
-            {exampleData.map((item, index) => (
+            
+            {user.map((item, index) => (
               <tr className="data-row">
-                <td className="ranking"> {index + 1}   <img src={item.picture} alt="defaultPicture"
+                <td className="ranking"> {index + 1} <img src={item.picture} alt="defaultPicture"
                   width="22" height="22" /> </td>
-                <td className="user-name"> {item.name} </td>
+                <td className="user-name"> {item.name.split(" ")[0]}</td>
                 <td className="score"> {item.score} </td>
               </tr>
             )
